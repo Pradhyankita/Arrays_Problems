c79b96ad74df33eeffba34538a32ef03c4d36b17
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-16 05:02:00 +0530

    Add: app test program

---
+++ src/com/bl/pp3/App.java
@@ -0,0 +1,11 @@
+package com.bl.pp3;
+
+public class App {
+    /**
+     * Hello world!
+   **/
+    public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+}
