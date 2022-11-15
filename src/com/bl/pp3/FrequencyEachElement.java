97c8f63eff563e916e3f399d4fe6304a7a2ad139
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-16 05:08:30 +0530

    Add:Frequency Of Each Element in Array

---
+++ src/com/bl/pp3/FrequencyEachElement.java
@@ -0,0 +1,27 @@
+package com.bl.pp3;
+
+import java.util.Scanner;
+
+public class FrequencyEachElement {
+
+    public static void main(String[] args) {
+        int[] a = {10, 50, 10, 60, 40, 70, 60};
+
+        for (int i=0; i<a.length; i++) {
+            int x = a[i];
+            int count = 0;
+
+            if (x == -1)continue;
+
+            for (int j=0; j<a.length; j++) {
+
+                if (a[j] == x) {
+                    count++;
+                    a[j] = -1;
+                }
+            }
+            System.out.println("Frequency of " +x+ " is " +count);
+        }
+    }
+
+}
