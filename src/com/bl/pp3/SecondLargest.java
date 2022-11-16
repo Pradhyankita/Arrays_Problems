5a211d94f551d621fe331cda4bf20dd72dcf0f57
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-16 05:33:05 +0530

    Add: second largest element of Array

---
+++ src/com/bl/pp3/SecondLargest.java
@@ -0,0 +1,20 @@
+package com.bl.pp3;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] a = {2, 6, 3, 4, 5, 1, 9};
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = i + 1; j < a.length; j++) {
+
+                if (a[i] < a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.println("Second largest Number is: " + a[1]);
+    }
+}
