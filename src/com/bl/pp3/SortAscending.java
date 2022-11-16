ca3fbe909e699a0d625459d051004fe3e29ed2ee
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-16 05:30:46 +0530

    Add: Sorting element of Array

---
+++ src/com/bl/pp3/SortAscending.java
@@ -0,0 +1,36 @@
+package com.bl.pp3;
+
+import java.util.Scanner;
+
+public class SortAscending {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        int temp;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Elements in Array: ");
+
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        for (int i=0; i<arr.length; i++) {
+
+            for (int j=i+1; j<arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sort Ascending order Elements:");
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]+" ");
+        }
+        scan.close();
+    }
+
+}
